7ba0d60f35cad43c3c0704865bcc9cded2c9d23c
gyojinnK <ksmug1998@gmail.com>
2024-06-28 17:38:35 +0900

    FIX: fix example goods

--- src/app/(navigator)/page.tsx
+++ src/app/(navigator)/page.tsx
@@ -13,7 +13,7 @@ export type TExampleGoods = {
 
 const exampleGoods: TExampleGoods = {
   id: "1",
-  title: "쌈뽕한 선글라스",
+  title: "데일리 선글라스",
   price: "3500",
   author: "경매의 신",
   tags: ["악세서리", "저렴한 시작가", "10대~20대", "안경/선글라스"],
